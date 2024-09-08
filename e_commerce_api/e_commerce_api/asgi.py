fb6a8c4c1bec55e0837ee5b18bb25f123d371372
Mohamed Bahgat <mbahgat503@gmail.com>
2024-09-08 19:30:26 +0300

    renaming

--- e_commerce_api/e_commerce_api/asgi.py
+++ e_commerce_api/e_commerce_api/asgi.py
@@ -1,5 +1,5 @@
 # """
-# ASGI config for my_api project.
+# ASGI config for e_commerce_api project.
 
 # It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@
 
 # from django.core.asgi import get_asgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_api.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_commerce_api.settings')
 
 # application = get_asgi_application()
 
@@ -21,7 +21,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from api.chat.routing import websocket_urlpatterns
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_commerce_api.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),  # HTTP protocol
